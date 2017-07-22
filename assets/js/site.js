90f5256484f3bf9e1ab98f6ae34733270654f5c3
Tobias Oetiker <tobi@oetiker.ch>
2017-07-23 00:26:15 +0200

    move menu to the left

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,8 @@
 jQuery(function() {
     var $menubutton = $('.menu-button');
     $menubutton.sideNav({
-            menuWidth: 300, // Default is 300
-            edge: 'right', // Choose the horizontal origin
+            menuWidth: 250, // Default is 300
+            edge: 'left', // Choose the horizontal origin
             closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
             draggable: true, // Choose whether you can drag to open on touch screens,
             onOpen: function(el) {
