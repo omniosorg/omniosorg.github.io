3ccde02692698bc58567751a7a9accd9e5dff2d6
Tobias Oetiker <tobi@oetiker.ch>
2017-07-22 21:54:04 +0200

    add site js

---
+++ assets/js/site.js
@@ -0,0 +1,15 @@
+jQuery(function() {
+    var $menubutton = $('.menu-button');
+    $menubutton.sideNav({
+            menuWidth: 300, // Default is 300
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true, // Choose whether you can drag to open on touch screens,
+            onOpen: function(el) {
+                $menubutton.addClass('is-active');
+            }, // A function to be called when sideNav is opened
+            onClose: function(el) {
+                $menubutton.removeClass('is-active');
+            } // A function to be called when sideNav is closed
+        });
+    });
