3eaa883e881cab0f225c503722ee7567cee1ccb0
Tobias Oetiker <tobi@oetiker.ch>
2017-07-23 14:53:50 +0200

    configure dropdown

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,5 +11,17 @@ jQuery(function() {
             onClose: function(el) {
                 $menubutton.removeClass('is-active');
             } // A function to be called when sideNav is closed
-        });
     });
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
+    
+});
