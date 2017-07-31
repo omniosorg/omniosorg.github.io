09990b64e3cbed08b5d471de0bcbe18783a062d1
Tobias Oetiker <tobi@oetiker.ch>
2017-07-31 22:50:50 +0200

    menu needs some more space

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,7 @@
 jQuery(function() {
     var $menubutton = $('.menu-button');
     $menubutton.sideNav({
-            menuWidth: 250, // Default is 300
+            menuWidth: 270, // Default is 300
             edge: 'left', // Choose the horizontal origin
             closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
             draggable: true, // Choose whether you can drag to open on touch screens,
